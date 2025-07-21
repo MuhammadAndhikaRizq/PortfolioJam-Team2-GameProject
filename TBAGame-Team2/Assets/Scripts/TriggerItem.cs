0380b20cbb3c8689a657fe4a447d13e78014163a
Muhammad Andhika Rizq <mandhikarizq@gmail.com>
2025-07-21 14:06:07 +0700

    Dialogue Next Trigger

---
+++ TBAGame-Team2/Assets/Scripts/TriggerItem.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using Fungus;
+using UnityEngine;
+
+public class TriggerItem : MonoBehaviour
+{
+    public Flowchart flowchart;
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Rokok"))
+        {
+            flowchart.SetBooleanVariable("isItemDelivered", true);
+            Destroy(other.gameObject);
+            flowchart.ExecuteBlock("Maya1SecondOrder");
+        }
+    }
+}
