283332694b073c3a74694ef6b534a6bb84011304
daffaalqmr <daffaalqomaro123@gmail.com>
2025-07-23 05:47:52 +0700

    Little Fix Bug Dialogue Repeat

---
+++ TBAGame-Team2/Assets/Scripts/PlasticBag.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+public class PlasticBag : MonoBehaviour
+{
+    [LabelText("List Item Type")]
+    public List<string> itemTypes = new List<string>();
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- TBAGame-Team2/Assets/Scripts/TriggerItem.cs
+++ TBAGame-Team2/Assets/Scripts/TriggerItem.cs
@@ -6,17 +6,20 @@ using UnityEngine;
 public class TriggerItem : MonoBehaviour
 {
     public Flowchart flowchart;
+    
 
     public void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Rokok"))
+        int status = flowchart.GetIntegerVariable("StoryState");
+        if (other.CompareTag("Rokok") && status == 0)
         {
             flowchart.SetBooleanVariable("isItemDelivered", true);
             Destroy(other.gameObject);
             flowchart.ExecuteBlock("Maya1SecondOrder");
+            flowchart.SetIntegerVariable("StoryState", 1);
         }
 
-        if (other.CompareTag("Juice"))
+        if (other.CompareTag("Juice") && status == 0)
         {
             flowchart.SetBooleanVariable("isItemDelivered", true);
             Destroy(other.gameObject);
