e7a25ce32119c9944a3efdd6d2ed07ddc25d0518
Muhammad Andhika Rizq <mandhikarizq@gmail.com>
2025-09-30 19:02:03 +0700

    Fix Item Scale (Only for ice cream)

--- TBAGame-Team2/Assets/Scripts/Trigger/ChangeSceneCollide.cs
+++ TBAGame-Team2/Assets/Scripts/Trigger/ChangeSceneCollide.cs
@@ -10,11 +10,11 @@ public class ChangeSceneCollide : MonoBehaviour
     public float scaleSpeed = 0.5f;
     public Vector3 targetScale = new Vector3(0.4f, 0.4f, 0.4f);
     private SwitchUI switchUI;
-    
 
     [Header("Item Setup")]
 
-    public string itemTag = "Item"; 
+    public string itemTag = "Item";
+    public string itemScale = "IceCream";
 
     void Start()
     {
@@ -30,14 +30,17 @@ public class ChangeSceneCollide : MonoBehaviour
     void OnTriggerExit2D(Collider2D other)
     {
       
-        if (other.CompareTag(itemTag))
+        if (other.CompareTag(itemTag) || other.CompareTag(itemScale))
         {
             Debug.Log("Item dengan tag '" + itemTag + "' telah keluar dari area. Mengganti scene...");
 
             if (targetScene != null && switchUI != null)
             {
                 other.transform.SetParent(targetScene.transform);
-                other.transform.localScale = targetScale;
+                if (other.CompareTag(itemScale))
+                {
+                    other.transform.localScale = targetScale;
+                }
                 switchUI.SwitchTo(targetScene);
             }
             else
